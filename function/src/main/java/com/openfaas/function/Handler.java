a2e1e9ff505f7051b05b1763e5a0b7725fb756f4
Ramsay Domloge <rdomloge@gmail.com>
2021-06-10 12:21:46 +0100

    Made output print out query params

--- function/src/main/java/com/openfaas/function/Handler.java
+++ function/src/main/java/com/openfaas/function/Handler.java
@@ -1,15 +1,26 @@
 package com.openfaas.function;
 
-import com.openfaas.model.IHandler;
-import com.openfaas.model.IResponse;
+import java.util.Map;
+import java.util.Map.Entry;
+
 import com.openfaas.model.IRequest;
+import com.openfaas.model.IResponse;
 import com.openfaas.model.Response;
 
 public class Handler extends com.openfaas.model.AbstractHandler {
 
     public IResponse Handle(IRequest req) {
+        
+        StringBuilder sb = new StringBuilder("Query\n");
+        Map<String, String> query = req.getQuery();
+        
+        for(Entry<String,String> entry : query.entrySet()) {
+            sb.append(entry.getKey()+": "+entry.getValue());
+            sb.append("\n");
+        }
+        
         Response res = new Response();
-	    res.setBody("Hello, world!");
+	    res.setBody(sb.toString());
 
 	    return res;
     }
