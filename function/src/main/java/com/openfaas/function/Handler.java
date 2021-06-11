f9ec0a311c3a5c10f0e6e7bb99e049f10569b564
Ramsay Domloge <rdomloge@gmail.com>
2021-06-11 14:58:36 +0100

    First draft of aggregation

--- function/src/main/java/com/openfaas/function/Handler.java
+++ function/src/main/java/com/openfaas/function/Handler.java
@@ -2,13 +2,23 @@ package com.openfaas.function;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
 import java.util.Map;
 
+import javax.management.RuntimeErrorException;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.openfaas.model.IRequest;
 import com.openfaas.model.IResponse;
 import com.openfaas.model.Response;
+
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeConstants;
 
 import okhttp3.Cache;
 import okhttp3.Call;
@@ -35,14 +45,16 @@ public class Handler extends com.openfaas.model.AbstractHandler {
             
             String fixtureJson = fetchFixture(query);
             JsonObject fixture = JsonParser.parseString(fixtureJson).getAsJsonObject();
-            System.out.println("Fixture deserialised: " + fixture.get("externalFixtureId").getAsInt());
 
-            int teamId = fixture.get("homeTeamId").getAsInt();
-            String clubJson = fetchClub(teamId);
-            JsonObject  club = JsonParser.parseString(clubJson).getAsJsonObject();
-            System.out.println("Deserialised club: "+club.get("clubName").getAsString());
+            JsonObject matchSession = fetchMatchSessionForFixture(
+                fixture.get("homeTeamId").getAsInt(), 
+                fixture.get("matchDate").getAsString());
 
-            res.setBody(fixtureJson);
+            JsonObject details = new JsonObject();
+            details.add("fixture", fixture);
+            details.add("session", matchSession);
+
+            res.setBody(details.getAsString());
         }
         catch(BadRequestException brex) {
             res = new Response();
@@ -63,6 +75,37 @@ public class Handler extends com.openfaas.model.AbstractHandler {
 
 	    return res;
     }
+
+
+    private static final SimpleDateFormat fixtureDateFormat = new SimpleDateFormat("YYYY-mm-DD");
+
+    private JsonObject fetchMatchSessionForFixture(int teamId, String fixtureDateStr) throws IOException, ParseException {
+        
+        DateTime fixtureDate = new DateTime(fixtureDateFormat.parse(fixtureDateStr).getTime());
+        
+        String clubJson = fetchClub(teamId);
+        JsonObject club = JsonParser.parseString(clubJson).getAsJsonObject();
+        JsonElement matchSessions = club.get("matchSessions");
+        JsonArray sessions = matchSessions.getAsJsonArray();
+        for(int i=0; i < sessions.size(); i++) {
+            JsonObject session = sessions.get(i).getAsJsonObject();
+            int dayOfWeek = daysAsJodaDayOfWeekInt(session.get("days").getAsString());
+            if(dayOfWeek == fixtureDate.getDayOfWeek()) return session;
+        }
+
+        throw new RuntimeException("Could not find a matching session");
+    }
+
+    public int daysAsJodaDayOfWeekInt(String days) {
+		if("Mondays".equalsIgnoreCase(days)) return DateTimeConstants.MONDAY;
+		if("Tuesdays".equalsIgnoreCase(days)) return DateTimeConstants.TUESDAY;
+		if("Wednesdays".equalsIgnoreCase(days)) return DateTimeConstants.WEDNESDAY;
+		if("Thursdays".equalsIgnoreCase(days)) return DateTimeConstants.THURSDAY;
+		if("Fridays".equalsIgnoreCase(days)) return DateTimeConstants.FRIDAY;
+		if("Saturdays".equalsIgnoreCase(days)) return DateTimeConstants.SATURDAY;
+		if("Sundays".equalsIgnoreCase(days)) return DateTimeConstants.SUNDAY;
+		throw new RuntimeException("Could not map "+days);
+	}
 
     private void checkRequest(Map<String, String> query) throws BadRequestException {
         if( ! query.containsKey(FIXTURE)) {
